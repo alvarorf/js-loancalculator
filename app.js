ab4b117d7e1d6bd2fb81d58e169e0d8b8522c067
alvaro <aaruizf@unal.edu.co>
2020-07-29 13:46:17 -0400

    Added functionality for spinner

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit',calculateResults);
-
-// Calculate results
-function calculateResults(e) {
-  console.log('Calculating...');
+document.getElementById('loan-form').addEventListener('submit', function(e){
+  // Hide results
+  document.getElementById('results').style.display = 'none';
   
-  // Get the UI vars
+  // Show loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
+
+// Calculate Results
+function calculateResults(){
+  console.log('Calculating...');
+  // UI Vars
   const amount = document.getElementById('amount');
   const interest = document.getElementById('interest');
   const years = document.getElementById('years');
@@ -14,40 +23,52 @@ function calculateResults(e) {
   const totalInterest = document.getElementById('total-interest');
 
   const principal = parseFloat(amount.value);
-  const calculatedInterest = parseFloat(interest.value)/100/12;
-  const calculatedPayments = parseFloat(years.value)*12;
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
 
   // Compute monthly payment
-  const x = Math.pow(1+calculatedInterest, calculatedPayments);
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
   const monthly = (principal*x*calculatedInterest)/(x-1);
+
   if(isFinite(monthly)) {
     monthlyPayment.value = monthly.toFixed(2);
-    totalPayment.value = (monthly*calculatedPayments).toFixed(2);
-    totalInterest.value = ((monthly*calculatedPayments)-principal).toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+
+    // Show results
+    document.getElementById('results').style.display = 'block';
+
+    // Hide loader
+    document.getElementById('loading').style.display = 'none';
+
   } else {
     showError('Please check your numbers');
   }
-
-
-  e.preventDefault();
 }
 
-//Show error
+// Show Error
 function showError(error){
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+  
+  // Hide loader
+  document.getElementById('loading').style.display = 'none';
+
   // Create a div
   const errorDiv = document.createElement('div');
 
   // Get elements
   const card = document.querySelector('.card');
   const heading = document.querySelector('.heading');
+
   // Add class
   errorDiv.className = 'alert alert-danger';
+
   // Create text node and append to div
   errorDiv.appendChild(document.createTextNode(error));
 
-  // Insert error above heading (we call it on the parent)
-  // parentelement.insertBefore(whatwewanttoinsert, whateverisbeforewhatwewanttoinsert)
-  card.insertBefore(errorDiv, heading); //Insert errorDiv before heading, as childs of card
+  // Insert error above heading
+  card.insertBefore(errorDiv, heading);
 
   // Clear error after 3 seconds
   setTimeout(clearError, 3000);
